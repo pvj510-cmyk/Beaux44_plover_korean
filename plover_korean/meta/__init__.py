915a0ffc2f926189031e2c3b29586b04d3a17dfe
pvj510-cmyk <pvj510@gmail.com>
2026-05-14 05:18:45 +0900

    Update __init__.py

--- plover_korean/meta/__init__.py
+++ plover_korean/meta/__init__.py
@@ -10,7 +10,7 @@ from plover_korean.meta.particle import (
     apply_particle_rang,
     apply_particle_na,
     apply_particle_ro,
-    apply_particle_myeo
+    apply_particle_myeo,
     undo_last_action,           # 추가됨
     apply_particle_terminal_n   # 추가됨
 )
